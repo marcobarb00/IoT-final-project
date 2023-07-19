3f7753bdbae2d6d06d1bffb189a7080389d8a495
user <marco10.barbieri@mail.polimi.it>
2023-07-19 15:15:24 +0200

    topology and structs

--- RadioRoute.h
+++ RadioRoute.h
@@ -4,12 +4,16 @@
 #define RADIO_ROUTE_H
 
 typedef nx_struct radio_route_msg {
-	//field 1
-	//field 2
-	//field 3
-	//....
-	//field n
+	nx_uint16_t type;
+	nx_uint16_t topic;
+	nx_uint16_t payload;
 } radio_route_msg_t;
+
+typedef nx_struct comunication_channel{
+	nx_uint16_t id;
+	nx_uint16_t status;
+	nx_uint16_t subscribed_topic;
+ } comunication_channel_t;
 
 enum {
   AM_RADIO_COUNT_MSG = 10,
--- RunSimulationScript.py
+++ RunSimulationScript.py
@@ -58,17 +58,18 @@ t.addChannel("radio_pack",out);
 
 
 
-print "Creating node 1...";
-node1 =t.getNode(1);
-time1 = 0*t.ticksPerSecond(); #instant at which each node should be turned on
-node1.bootAtTime(time1);
-print ">>>Will boot at time",  time1/t.ticksPerSecond(), "[sec]";
+print "Creating PANC, node:" + str(i) + "...";
+node =t.getNode(1);
+time = 0*t.ticksPerSecond(); #instant at which each node should be turned on
+node.bootAtTime(time);
+print ">>>Will boot at time",  time/t.ticksPerSecond(), "[sec]";
 
-print "Creating node 2...";
-node2 = t.getNode(2);
-time2 = 0*t.ticksPerSecond();
-node2.bootAtTime(time2);
-print ">>>Will boot at time", time2/t.ticksPerSecond(), "[sec]";
+for i in range(2, 9):
+  print "Creating node" + str(i) + "...";
+  node =t.getNode(i);
+  time = 0*t.ticksPerSecond(); #instant at which each node should be turned on
+  node.bootAtTime(time);
+  print ">>>Will boot at time",  time/t.ticksPerSecond(), "[sec]";
 
 print "Creating radio channels..."
 f = open(topofile, "r");
@@ -99,11 +100,11 @@ for line in lines:
             mid_compl = 0;
             sys.stdout.write ("#")
             sys.stdout.flush()
-        for i in range(1, 8):
+        for i in range(1, 9):
             t.getNode(i).addNoiseTraceReading(val)
 print "Done!";
 
-for i in range(1, 8):
+for i in range(1, 9):
     print ">>>Creating noise model for node:",i;
     t.getNode(i).createNoiseModel()
 
